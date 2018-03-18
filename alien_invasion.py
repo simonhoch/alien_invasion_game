245caa3775f832dc0317878e9f2c66b026ecffd2
simonhoch <simonhoch1@gmail.com>
2018-03-18 14:09:24 +0200

    made the ship fired bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,2 +1,25 @@
-for n in range (1, 3):
-    print('*')
+import sys
+
+import pygame
+
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+def run_game():
+    # Initialize game and create  screen object.
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+            (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    # Make a ship.
+    ship = Ship(screen)
+
+    # Start the main loop for the game.
+    while True:
+        gf.check_events()
+        gf.update_screen(ai_settings, screen, ship)
+
+run_game()
