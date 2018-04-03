dc5e150fe173ab74bb3b2bc8a80f082b8974863c
simonhoch <simonhoch1@gmail.com>
2018-04-03 21:57:37 +0300

    Added the beggining of the scoring of the game.

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,6 +7,7 @@ from settings import Settings
 from ship import Ship
 from alien import Alien
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from button import Button
 
 import game_functions as gf
@@ -22,8 +23,9 @@ def run_game():
     # Make the Play button.
     play_button = Button(ai_settings, screen, "Play")
 
-    # Create an instance to store game statistics.
+    # Create an instance to store game statistics and create a scoreboard.
     stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
     # Make a ship, a group of bullets, and a group of aliens.
     ship = Ship(ai_settings, screen)
     bullets = Group()
@@ -40,9 +42,10 @@ def run_game():
         if stats.game_active:
             ship.update()
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
-            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens,
+                    bullets)
 
-        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets,
-                play_button)
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens,
+                bullets,play_button)
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -51,6 +51,9 @@ def check_play_button(ai_settings,screen, stats, play_button, ship, aliens,
     """Start a new game when the player click Play."""
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
+        # Reset the game settings.
+        ai_settings.initialize_dynamic_settings()
+
         # Hide the mouse cursor.
         pygame.mouse.set_visible(False)
 
@@ -66,7 +69,7 @@ def check_play_button(ai_settings,screen, stats, play_button, ship, aliens,
         create_fleet(ai_settings, screen, ship, aliens)
         ship.center_ship()
 
-def update_screen(ai_settings, screen, stats, ship, aliens, bullets,
+def update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets,
         play_button):
     """Update images on the screen and flip to the new screen."""
     # Redraw the screen during each pass through the loop.
@@ -78,6 +81,9 @@ def update_screen(ai_settings, screen, stats, ship, aliens, bullets,
 
     ship.blitme()
     aliens.draw(screen)
+
+    # Draw the score information.
+    sb.show_score()
 
     # Draw the play button if the game iis inactive.
     if not stats.game_active:
@@ -104,8 +110,9 @@ def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
     if len(aliens) == 0:
-        # Destroy existing bullets and create new fleet.
+        # Destroy existing bullets, speedup game, and create new fleet.
         bullets.empty()
+        ai_settings.increase_speed()
         create_fleet(ai_settings, screen, ship, aliens)
 
 def get_number_aliens_x(ai_settings,alien_width):
